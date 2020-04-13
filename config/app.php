d8408cd2389b064947bd8c3fda50abf6a2d2ee8e
orbachinujbuk <orbachinujbuk@gmail.com>
2020-04-14 03:43:59 +0600

    asd3

--- config/app.php
+++ config/app.php
@@ -163,7 +163,8 @@ return [
         Collective\Html\HtmlServiceProvider::class,
         niklasravnsborg\LaravelPdf\PdfServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
-        Mews\Purifier\PurifierServiceProvider::class
+        Mews\Purifier\PurifierServiceProvider::class,
+        Riverskies\Laravel\MobileDetect\MobileDetectServiceProvider::class
 
 
     ],
@@ -217,7 +218,8 @@ return [
         'Html' => Collective\Html\HtmlFacade::class,
         'PDF' => niklasravnsborg\LaravelPdf\Facades\Pdf::class,
         'Image' => Intervention\Image\Facades\Image::class,
-        'Purifier' => Mews\Purifier\Facades\Purifier::class
+        'Purifier' => Mews\Purifier\Facades\Purifier::class,
+        'MobileDetect' => Riverskies\Laravel\MobileDetect\Facades\MobileDetect::class
 
     ],
 
