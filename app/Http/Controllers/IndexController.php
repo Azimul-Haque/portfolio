587fbb631f00ea637be836484c513f6541805ddb
orbachinujbuk <orbachinujbuk@gmail.com>
2020-04-16 04:42:49 +0600

    ADS3

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -267,7 +267,7 @@ class IndexController extends Controller
     // clear configs, routes and serve
     public function clear()
     {
-        Artisan::call('optimize');
+        // Artisan::call('optimize');
         Artisan::call('cache:clear');
         Artisan::call('view:clear');
         Artisan::call('key:generate');
