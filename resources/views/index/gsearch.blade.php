0c8b0eec47016dd9582cd2215550cb4020fc2ffd
orbachinujbuk <orbachinujbuk@gmail.com>
2020-04-16 04:12:36 +0600

    ASD33

--- resources/views/index/gsearch.blade.php
+++ resources/views/index/gsearch.blade.php
@@ -9,38 +9,38 @@
 
 @section('content')
     <!-- head section -->
-      <section class="content-top-margin page-title page-title-small bg-gray">
-          <div class="container">
-              <div class="row">
-                  <div class="col-lg-8 col-md-7 col-sm-12 wow fadeInUp" data-wow-duration="300ms">
-                      <!-- page title -->
-                      <h1 class="black-text">Search</h1>
-                      <!-- end page title -->
-                  </div>
-                  <div class="col-lg-4 col-md-5 col-sm-12 breadcrumb text-uppercase wow fadeInUp xs-display-none" data-wow-duration="600ms">
-                      <!-- breadcrumb -->
-                      <ul>
-                          <li><a href="{{ route('index.index') }}">Home</a></li>
-                          <li><a href="#">About</a></li>
-                          <li>Journey</li>
-                      </ul>
-                      <!-- end breadcrumb -->
-                  </div>
-              </div>
-          </div>
-      </section>
-      <!-- end head section -->
-      <!-- WHATWEDO section -->
-      <section class="border-bottom-light sm-text-center" style="min-height: 500px;">
-          <div class="container">
-              <div class="row">
-                  <div class="col-md-12 wow fadeInUp" data-wow-duration="400ms">
-                    <div class="gcse-search" data-queryParameterName="search"></div>
-                  </div>
-              </div>
-          </div>
-      </section>
-      <!-- end WHATWEDO section -->
+    <section class="content-top-margin page-title page-title-small bg-gray">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-md-7 col-sm-12 wow fadeInUp" data-wow-duration="300ms">
+                    <!-- page title -->
+                    <span class="text-large letter-spacing-2 black-text font-weight-600 text-uppercase agency-title">Search</span>
+                    <!-- end page title -->
+                </div>
+                <div class="col-lg-4 col-md-5 col-sm-12 breadcrumb text-uppercase wow fadeInUp xs-display-none" data-wow-duration="600ms">
+                    <!-- breadcrumb -->
+                    <ul @desktop class="text-right" @enddesktop>
+                        <li><a href="{{ route('index.index') }}">Home</a></li>
+                        <li><a href="#">Search</a></li>
+                    </ul>
+                    <!-- end breadcrumb -->
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- end head section -->
+    
+    <!-- WHATWEDO section -->
+    <section class="border-bottom-light sm-text-center" style="min-height: 500px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 wow fadeInUp" data-wow-duration="400ms">
+                  <div class="gcse-search" data-queryParameterName="search"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- end WHATWEDO section -->
 @endsection
 
 @section('js')
