5cb0444c7f961cb3c8d8b8526106b12d21ccf732
orbachinujbuk <orbachinujbuk@gmail.com>
2020-04-16 00:05:20 +0600

    ASd33

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -56,17 +56,8 @@
     <!-- end quotes section -->
 
     <!-- about section -->
-    <section class="no-padding-bottom wow fadeIn">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 col-sm-10 text-center center-col">
-                    <span class="margin-five no-margin-top display-block letter-spacing-2">EST. 2016</span>
-                    <h1>IITDU Alumni Association</h1>
-                    <p class="text-med width-90 center-col margin-seven no-margin-bottom"> We've been crafting beautiful websites, launching stunning brands and making clients happy for years. With our prestigious craftsmanship, remarkable client care and passion for design.</p>
-                </div>
-            </div>
-        </div>
-        <div class="container-fluid margin-five no-margin-bottom">
+    <section class="wow fadeIn no-padding">
+        <div class="container-fluid">
             <div class="row">
                 <div class="col-md-12 col-sm-12 col-xs-12 bg-fast-yellow padding-three text-center">
                     <span class="text-small text-uppercase font-weight-600 black-text letter-spacing-2">Professionalism &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Excellence &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Respect</span>
@@ -74,22 +65,9 @@
             </div>
         </div>
     </section>
-    <!-- end about section -->
-    <section class="wow fadeInUp bg-gray">
-        <div class="container">
-            <div class="row">
-                <!-- call to action -->
-                <div class="col-md-7 col-sm-12 text-center center-col">
-                    <p class="title-large text-uppercase letter-spacing-1 black-text font-weight-600 wow fadeIn">Want to be a memeber of IITAluni?</p>
-                    <a href="{{ route('index.application') }}" class="highlight-button-black-border btn margin-six wow fadeInUp">Apply Now!</a>
-                </div>
-                <!-- end call to action -->
-            </div>
-        </div>
-    </section>
 
     <!-- counter section -->
-    <section id="counter" class="fix-background" style="background-image:url('images/slider/slider-img45.jpg');">
+    <section id="counter" class="fix-background" style="background-image:url('/images/home_page_back_2.jpg');">
         <div class="opacity-full bg-dark-gray"></div>
         <div class="container position-relative">
             <div class="row">
