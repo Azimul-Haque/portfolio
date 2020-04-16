2bf8bfd5c9379993b71bd213a8f76255a6c93e05
orbachinujbuk <orbachinujbuk@gmail.com>
2020-04-17 04:38:27 +0600

    ASD33

--- resources/views/index/multimedia.blade.php
+++ resources/views/index/multimedia.blade.php
@@ -91,27 +91,8 @@
                     {{-- <a href="#!" class="blog-like"><i class="fa fa-heart-o"></i>{{ $blog->likes }} Like(s)</a> --}}
                     <div class="fb-like blog-like" data-href="{{ url('/multimedia/'.$blog->slug) }}" data-width="" data-layout="button_count" data-action="like" data-size="small" data-share="false" ></div>
 
-                    <a href="#!" class="comment"><i class="fa fa-comment-o"></i>
-                      {{-- <span id="comment_count{{ $blog->id }}"></span> --}}
-                      <span class="fb-comments-count" data-href="{{ url('/multimedia/'.$blog->slug) }}">0</span> comment(s)</a>
+                    <a href="{{ url('/multimedia/'.$blog->slug . '#comment_section') }}" class="comment"><i class="fa fa-comment-o"></i><span class="fb-comments-count" data-href="{{ url('/multimedia/'.$blog->slug) }}">0</span> comment(s)</a>
                   </div>
-                  {{-- <script type="text/javascript" src="{{ asset('vendor/hcode/js/jquery.min.js') }}"></script>
-                  <script type="text/javascript">
-                      $.ajax({
-                          url: "https://graph.facebook.com/v2.2/?fields=share{comment_count}&id={{ url('/blog/'.$blog->slug) }}",
-                          dataType: "jsonp",
-                          success: function(data) {
-                              if(data.hasOwnProperty('share')) {
-                                $('#comment_count{{ $blog->id }}').text(data.share.comment_count);
-                              } else {
-                                $('#comment_count{{ $blog->id }}').text(0);
-                              }
-                          },
-                          error: function(data) {
-                            
-                          }
-                      });
-                  </script> --}}
               </div>
           </div>
           @php $counter++; @endphp
