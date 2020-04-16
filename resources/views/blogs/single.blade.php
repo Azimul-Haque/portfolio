b1c86f22e4bf6df356d37fe5845acab3ad85b648
orbachinujbuk <orbachinujbuk@gmail.com>
2020-04-17 04:10:51 +0600

    asd3

--- resources/views/blogs/single.blade.php
+++ resources/views/blogs/single.blade.php
@@ -122,8 +122,7 @@
 
                         <div class="separator-line bg-black no-margin-lr margin-four"></div>
                         <div>
-                            <div class="fb-like blog-like" data-href="{{ Request::url() }}" data-width="" data-layout="button_count" data-action="like" data-size="small" data-share="false"></div>  <span class="fb-like-count" data-href="{{ Request::url() }}">0</span> <span class="fb-like-counts" data-href="{{ Request::url() }}">0</span>
-                            <fb:like-count href="{{ Request::url() }}"></fb:like-count>
+                            <div class="fb-like blog-like" data-href="{{ Request::url() }}" data-width="" data-layout="button_count" data-action="like" data-size="small" data-share="false"></div>  <span class="_5n6h _2pih" data-href="{{ Request::url() }}">0</span> <span class="fb-like-counts" data-href="{{ Request::url() }}">0</span>
                                                      
                             {{-- <a href="#!" class="blog-like" @if(Auth::check()) onclick="likeBlog({{ Auth::user()->id }}, {{ $blog->id }})" @else title="Login to Like!" @endif>
                                 <i class="fa fa-heart-o" id="like_icon"></i>
@@ -150,19 +149,6 @@
                                     }
                                 });
                             </script> --}}
-                            <script type="text/javascript">
-                                $.ajax({
-                                    url: "https://graph.facebook.com/v2.2/?fields=share{like_count}&id={{ Request::url() }}",
-                                    dataType: "jsonp",
-                                    success: function(data) {
-                                        if(data.share) {
-                                            $('#comment_count').text(data.share.like_count);
-                                        } else {
-                                            $('#comment_count').text(0);
-                                        }
-                                    }
-                                });
-                            </script>
                         </div>
                         <!-- end post tags -->
                     </div>
