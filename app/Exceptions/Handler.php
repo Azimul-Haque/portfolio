e6b95e19e6f23fe352211ee4140507c99439fe9a
orbachinujbuk <orbachinujbuk@gmail.com>
2020-04-18 03:57:44 +0600

    ASd33

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -45,6 +45,21 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
+        // 404 page when a model is not found
+        if ($e instanceof ModelNotFoundException) {
+            return response()->view('errors.404', [], 404);
+        }
+
+        // 404 page if ANY error occurs!
+        // if ($e instanceof Exception) {
+        //     return response()->view('errors.404', [], 404);
+        // }
+
+        // Custom error 500 view on production
+        if (app()->environment() == 'production') {
+            return response()->view('errors.500', [], 500);
+        }
+
         return parent::render($request, $e);
     }
 }
