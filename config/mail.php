c9981ad8ae6b0452c77fb8c65ce56930b6764b8d
orbachinujbuk <orbachinujbuk@gmail.com>
2020-04-24 01:00:12 +0600

    ASD33

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'in-v3.mailjet.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'support@atiqueriyad.com', 'name' => 'Atique Riyad'],
 
     /*
     |--------------------------------------------------------------------------
@@ -108,5 +108,4 @@ return [
     */
 
     'sendmail' => '/usr/sbin/sendmail -bs',
-
 ];
