93024a1599d6ec21a58198fc0353d11db3a03a0a
orbachinujbuk <orbachinujbuk@gmail.com>
2020-04-30 17:44:53 +0600

    ASD4234

--- resources/views/dashboard/blogs/index.blade.php
+++ resources/views/dashboard/blogs/index.blade.php
@@ -38,7 +38,7 @@
               @else
                 <span class="badge"><i class="fa fa-bell-slash-o"></i> Unpublished</span>
               @endif
-              
+                <span class="badge"><i class="fa fa-eye"></i> {{ $blog->hits }}</span>
             </td>
             <td><span class="label label-success">{{ $blog->category->name }}</span></td>
             @handheld
